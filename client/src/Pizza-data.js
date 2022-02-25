4a52619a084bc9b9c06e19d6303d4a5d0b08babb
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-02-25 16:24:35 +0530

    Create Pizza-data.js

---
+++ client/src/Pizza-data.js
@@ -0,0 +1,174 @@
+ const pizzas = [
+  {
+    name: "PEPPER BARBECUE CHICKEN",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "Non-Veg",
+    image:
+      "https://5.imimg.com/data5/HW/SD/MY-29540739/pepper-barbecue-chicken-500x500.png",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "TANDOORI PANEER",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 150,
+        medium: 300,
+        large: 399,
+      },
+    ],
+    category: "Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/cijuato5w3n7a28jc4vg",
+    description:
+      "Spiced paneer, onion, green capsicum & red paprika in tandoori sauce",
+  },
+  {
+    name: "TRIPLE CHICKEN FEAST",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Non-Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/flrbyptxa374zsgocajm",
+    description:
+      "Schezwan chicken meatball, herbed chicken, chicken sausage, green capsicum, onion & red paprika",
+  },
+  {
+    name: "VEGGIE SUPREME",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k",
+    description:
+      "Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
+  },
+  {
+    name: "CHICKEN SUPREME",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Non-Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/mkiopegydp504qadvovm",
+    description: "Herbed chicken, schezwan chicken meatball & chicken tikka",
+  },
+  {
+    name: "CHICKEN TIKKA",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Non-Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xt8zrp5gn4s76nkxzcuu",
+    description: "Chicken Tikka chunks topped with tomatoes |onions | Cheese",
+  },
+  {
+    name: "FARMERS PICK",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/bbqw7rozorossijr2oub",
+    description:
+      "Herbed onion & green capsicum, red capsicum, mushroom & baby corn",
+  },
+  {
+    name: "DOUBLE CHECKEN SAUSAGE",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Non-Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/zcnopszrh1or7inty4xs",
+    description: "Chicken Sausage",
+  },
+  {
+    name: "CHICKEN PEPPER CRUNCH",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Non-Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/nn5rawqdo7mfia6kihrp",
+    description: "Herbed chicken, onion, green capsicum & red capsicum",
+  },
+  {
+    name: "PANEER & GREEN CAPSICUM",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 250,
+        medium: 390,
+        large: 499,
+      },
+    ],
+    category: "Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/gnohld1ffqmzgcoxo8i4",
+    description:
+      "Flavorful soft Paneer & Crunchy Green Capsicum for the perfect balance.",
+  },
+  {
+    name: "MARGHERITA",
+    varients: ["small", "medium", "large"],
+    price: [
+      {
+        small: 150,
+        medium: 290,
+        large: 350,
+      },
+    ],
+    category: "Veg",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/d22idtuzffj7fogkgxx2",
+    description: "Nothing but cheese!",
+  },
+];
+ 
+export default pizzas;
