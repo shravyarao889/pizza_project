c35d5dce4d7e9c15700e177799f4db5f274395a8
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-02-25 16:24:44 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Homescreen from "./screens/Homescreen";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homescreen />
     </div>
   );
 }
