cb2a1f8aec943339dd0916b6b172ab73b036073e
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-02-25 12:10:59 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
